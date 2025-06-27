0988571c1e9c34b2f2291846b2b197e4d6791838
katrinshtorm <77185151+katrinshtorm@users.noreply.github.com>
2025-06-27 16:23:49 +0900

    Add files via upload

---
+++ convert_ankiapp_xml.py
@@ -0,0 +1,20 @@
+import xml.etree.ElementTree as ET
+import re
+
+def extract_text_from_richtext(elem):
+    text_parts = []
+    for part in elem.iter():
+        if part.text:
+            text_parts.append(part.text.strip())
+    return ' / '.join(filter(None, text_parts))
+
+tree = ET.parse('ankiapp_export.xml')  # замени на имя своего XML-файла, если другое
+root = tree.getroot()
+
+with open('anki_import.txt', 'w', encoding='utf-8') as f:
+    for card in root.findall('.//card'):
+        front_elem = card.find(".//rich-text[@name='Front']")
+        back_elem = card.find(".//rich-text[@name='Back']")
+        front = extract_text_from_richtext(front_elem)
+        back = extract_text_from_richtext(back_elem)
+        f.write(f"{front}\t{back}\n")  # табуляция между полями
